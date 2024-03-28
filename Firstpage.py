0d2f38857f82cab50908aedcbc45acc9d8e92548
Priyankasarwar <priyankasarwar344@gmail.com>
2024-03-28 21:48:27 +0530

    firstcommit

---
+++ Firstpage.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title('REVERSED DCF')
+st.write('This site provides interactive tools to valuate and analyze stocks through Reverse DCF model. Check the navigation bar for more.')
